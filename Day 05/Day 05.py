250fe45c578b03ead8c017fe875ada0f62690037
Gert Jan <bakkerjangert@hotmail.com>
2022-12-05 11:41:44 +0100

    update

--- Day 05/Day 05.py
+++ Day 05/Day 05.py
@@ -4,57 +4,68 @@ from copy import deepcopy
 # https://adventofcode.com/2022/day/5
 # Read input data
 
+
 def print_dict_pt1():
     for i in range(1, no_rows + 1):
-        print(crates[i])
+        print(crates_pt1[i])
+
 
 def print_dict_pt2():
     for i in range(1, no_rows + 1):
         print(crates_pt2[i])
 
 tic = time.perf_counter()
-with open('input.txt') as f:
+PRINT_STEPS = False
+file = 'input.txt'
+# file = 'test.txt'
+
+with open(file) as f:
     lines = f.read().splitlines()
-no_rows = 9
-crates = dict()
+layout, instructions, no_rows = [], [], None
+for line in lines:
+    if '[' in line:
+        layout.append(line)
+    if 'move' in line:
+        instructions.append(line)
+    if '   ' in line and '[' not in line:
+        while line[-1] == ' ':
+            line = line[:-1]
+        no_rows = int(line.split(' ')[-1])
+
+crates_pt1 = dict()
 for i in range(1, no_rows + 1):
-    crates[i] = []
-layout = lines[0:8]
-instructions = lines[10:]
+    crates_pt1[i] = []
 for line in layout[::-1]:
-    for i in range(9):
-        if line[1 + i * 4] != ' ':
-            crates[i + 1].append(line[1 + i * 4])
+    for i in range(no_rows):
+        if len(line) > i * 4:
+            if line[1 + i * 4] != ' ':
+                crates_pt1[i + 1].append(line[1 + i * 4])
 
-# Test values
-# no_rows = 3
-# crates = {1: ['Z', 'N'],
-#           2: ['M', 'C', 'D'],
-#           3: ['P']}
-# instructions = ['move 1 from 2 to 1', 'move 3 from 1 to 3', 'move 2 from 2 to 1', 'move 1 from 1 to 2']
-
-crates_pt2 = deepcopy(crates)
+crates_pt2 = deepcopy(crates_pt1)
 
 # Part 1
-# print('Start configuration part 1')
-# print_dict()
+if PRINT_STEPS:
+    print('Start configuration part 1')
+    print_dict_pt1()
 for line in instructions:
     number_of_crates = int(line.split(' ')[1])
     from_row = int(line.split(' ')[3])
     to_row = int(line.split(' ')[5])
-    moved_crates = crates[from_row][-number_of_crates:]
-    crates[from_row] = crates[from_row][:-number_of_crates]
-    crates[to_row] = crates[to_row] + moved_crates[::-1]
-    # print('\nAfter next move')
-    # print_dict()
-
+    moved_crates = crates_pt1[from_row][-number_of_crates:]
+    crates_pt1[from_row] = crates_pt1[from_row][:-number_of_crates]
+    crates_pt1[to_row] = crates_pt1[to_row] + moved_crates[::-1]
+    if PRINT_STEPS:
+        print('\nAfter next move')
+        print_dict_pt1()
+print('\nThe answer to part 1:')
 for i in range(1, no_rows + 1):
-    print(crates[i][-1], end='')
+    print(crates_pt1[i][-1], end='')
 print('')
 
 # Part 2
-# print('Start configuration part 2')
-# print_dict_pt2()
+if PRINT_STEPS:
+    print('\nStart configuration part 2')
+    print_dict_pt2()
 
 for line in instructions:
     number_of_crates = int(line.split(' ')[1])
@@ -63,13 +74,13 @@ for line in instructions:
     moved_crates = crates_pt2[from_row][-number_of_crates:]
     crates_pt2[from_row] = crates_pt2[from_row][:-number_of_crates]
     crates_pt2[to_row] = crates_pt2[to_row] + moved_crates
-    # print('\nAfter next move')
-    # print_dict_pt2()
-
+    if PRINT_STEPS:
+        print('\nAfter next move')
+        print_dict_pt2()
+print('\nThe answer to part 2:')
 for i in range(1, no_rows + 1):
     print(crates_pt2[i][-1], end='')
 print('')
 
 toc = time.perf_counter()
-
 print(f'\nCalculation ended in {toc - tic:0.4f} seconds')
